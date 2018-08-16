c1d85ebfbaf6db336f1cfc1d6cd82980bdb08f4f
vicariusxz <cris_chong2@hotmail.com>
2018-08-16 16:41:05 +0300

    12

--- plugins/cordova-universal-links-plugin/hooks/lib/ios/xcodePreferences.js
+++ plugins/cordova-universal-links-plugin/hooks/lib/ios/xcodePreferences.js
@@ -1,10 +1,3 @@
-/*
-Script activates support for Universal Links in the application by setting proper preferences in the xcode project file.
-Which is:
-- deployment target set to iOS 9.0
-- .entitlements file added to project PBXGroup and PBXFileReferences section
-- path to .entitlements file added to Code Sign Entitlements preference
-*/
 
 var path = require('path');
 var compare = require('node-version-compare');
@@ -135,17 +128,23 @@ function isPbxReferenceAlreadySet(fileReferenceSection, entitlementsRelativeFile
 function loadProjectFile() {
   var platform_ios;
   var projectFile;
-
   try {
     // try pre-5.0 cordova structure
     platform_ios = context.requireCordovaModule('cordova-lib/src/plugman/platforms')['ios'];
     projectFile = platform_ios.parseProjectFile(iosPlatformPath());
   } catch (e) {
-    // let's try cordova 5.0 structure
-    platform_ios = context.requireCordovaModule('cordova-lib/src/plugman/platforms/ios');
-    projectFile = platform_ios.parseProjectFile(iosPlatformPath());
+    try {
+      // let's try cordova 5.0 structure
+      platform_ios = context.requireCordovaModule('cordova-lib/src/plugman/platforms/ios');
+      projectFile = platform_ios.parse(iosPlatformPath());
+    } catch (e) {
+      // try cordova 7.0 structure
+      var iosPlatformApi = require(path.join(iosPlatformPath(), '/cordova/Api'));
+      var projectFileApi = require(path.join(iosPlatformPath(), '/cordova/lib/projectFile.js'));
+      var locations = (new iosPlatformApi()).locations;
+      projectFile = projectFileApi.parse(locations);
+    }
   }
-
   return projectFile;
 }
 
@@ -187,5 +186,3 @@ function pathToEntitlementsFile() {
 
   return path.join(projectName, 'Resources', fileName);
 }
-
-// endregion
